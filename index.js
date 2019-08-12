846acf3535b598ead3c9af88c1f974080e94f89b
rahulshahaha <getrshah@gmail.com>
2019-08-11 22:14:41 -0400

    Added line chart

--- index.js
+++ index.js
@@ -7,285 +7,218 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var Card = function (_React$Component) {
-  _inherits(Card, _React$Component);
+    _inherits(Card, _React$Component);
 
-  function Card() {
-    _classCallCheck(this, Card);
+    function Card() {
+        _classCallCheck(this, Card);
 
-    return _possibleConstructorReturn(this, (Card.__proto__ || Object.getPrototypeOf(Card)).apply(this, arguments));
-  }
-
-  _createClass(Card, [{
-    key: "render",
-    value: function render() {
-      return React.createElement(
-        "div",
-        { className: "card" },
-        React.createElement(
-          "h1",
-          { className: "companyName" },
-          this.props.name
-        ),
-        React.createElement(
-          "h6",
-          { className: "currentPrice" },
-          "$",
-          this.props.price
-        ),
-        React.createElement(
-          "h6",
-          { className: this.props.changeType },
-          "\xA0 (",
-          this.props.percentChange,
-          "%)"
-        ),
-        React.createElement(
-          "div",
-          null,
-          React.createElement(App, { chartData: this.props.chartData })
-        )
-      );
+        return _possibleConstructorReturn(this, (Card.__proto__ || Object.getPrototypeOf(Card)).apply(this, arguments));
     }
-  }]);
 
-  return Card;
-}(React.Component);
-
-var Deck = function (_React$Component2) {
-  _inherits(Deck, _React$Component2);
-
-  function Deck(props) {
-    _classCallCheck(this, Deck);
-
-    var _this2 = _possibleConstructorReturn(this, (Deck.__proto__ || Object.getPrototypeOf(Deck)).call(this, props));
-
-    _this2.state = {
-      stocks: [{ ticker: "sfdfsd", name: "Very Long Company Name Incorporated", price: 123, percentChange: 4, changeType: "percentChangeUp" }, { ticker: "W", name: "Wayfair, Inc.", price: 123, percentChange: -2, changeType: "percentChangeDown" }, { ticker: "B", name: "B", price: 123 }, { ticker: "C", name: "C", price: 123 }, { ticker: "D", name: "D", price: 123 }, { ticker: "E", name: "E", price: 123 }, { ticker: "F", name: "F", price: 123 }, { ticker: "G", name: "G", price: 123 }]
-    };
-    _this2.getStockData();
-    return _this2;
-  }
-
-  _createClass(Deck, [{
-    key: "getStockData",
-    value: function getStockData() {
-      var _this3 = this;
-
-      var symbols = "w,f,ba,aapl,fb,jnj";
-
-      var Http = new XMLHttpRequest();
-      var url = 'https://cloud.iexapis.com/stable/stock/market/batch?symbols=' + symbols + '&types=quote,chart&range=1d&token=pk_ea3fad39b66c4c08a98acce72eda2aaa';
-      Http.open("GET", url);
-
-      Http.onload = function (e) {
-        var stockData = JSON.parse(Http.responseText);
-        console.log(stockData);
-        var stocks = [];
-        for (var key in stockData) {
-          var name = stockData[key].quote.companyName;
-          var price = stockData[key].quote.latestPrice;
-          var ticker = stockData[key].quote.symbol;
-          var percentChange = (stockData[key].quote.latestPrice - stockData[key].quote.open) / stockData[key].quote.open;
-          percentChange = Math.round(percentChange * 100 * 100) / 100;
-          var changeType = percentChange >= 0 ? "percentChangeUp" : "percentChangeDown";
-          var chartData = [];
-          for (var key2 in stockData[key].chart) {
-            var dateKey;
-            dateKey = stockData[key].chart[key2].date + " " + stockData[key].chart[key2].minute;
-            //console.log(Date.parse(stockData[key].chart[key2].date));
-            //console.log(Date.parse(dateKey));
-            chartData.push({ x: Date.parse(dateKey), y: stockData[key].chart[key2].close });
-          }
-          console.log(chartData);
-          var b = { name: name, price: price, ticker: ticker, percentChange: percentChange, changeType: changeType, chartData: chartData };
-          stocks.push(b);
+    _createClass(Card, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                { className: "card" },
+                React.createElement(
+                    "h1",
+                    { className: "companyName" },
+                    this.props.name
+                ),
+                React.createElement(
+                    "h6",
+                    { className: "currentPrice" },
+                    "$",
+                    this.props.price
+                ),
+                React.createElement(
+                    "h6",
+                    { className: this.props.changeType },
+                    "\xA0 (",
+                    this.props.percentChange,
+                    "%)"
+                ),
+                React.createElement(Chart, { chartData: this.props.chartData })
+            );
         }
-        _this3.setState({ stocks: stocks });
-      };
+    }]);
 
-      Http.send();
-    }
-  }, {
-    key: "generateCards",
-    value: function generateCards() {
-      var stateValues = this.state.stocks;
-      var cardsList = stateValues.map(function (s) {
-        return React.createElement(Card, { key: s.ticker, name: s.name, price: s.price, percentChange: s.percentChange, changeType: s.changeType, chartData: s.chartData });
-      });
-
-      return cardsList;
-    }
-  }, {
-    key: "render",
-    value: function render() {
-      return React.createElement(
-        "div",
-        { className: "deck" },
-        this.generateCards()
-      );
-    }
-  }]);
-
-  return Deck;
+    return Card;
 }(React.Component);
 
-var App = function (_React$Component3) {
-  _inherits(App, _React$Component3);
+var Chart = function (_React$Component2) {
+    _inherits(Chart, _React$Component2);
 
-  function App() {
-    _classCallCheck(this, App);
+    function Chart() {
+        _classCallCheck(this, Chart);
 
-    return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).apply(this, arguments));
-  }
-
-  _createClass(App, [{
-    key: "createFakeData",
-    value: function createFakeData() {
-      // This function creates data that doesn't look entirely random
-      var data = [];
-      for (var x = 0; x <= 30; x++) {
-        var random = Math.random();
-        var temp = data.length > 0 ? data[data.length - 1].y : 50;
-        var y = random >= .45 ? temp + Math.floor(random * 20) : temp - Math.floor(random * 20);
-        data.push({ x: x, y: y });
-      }
-      // data = [
-      //     {x:0, y:0},
-      //     {x:1,y:1},
-      //     {x:3,y:-1}
-      // ]
-      return data;
+        return _possibleConstructorReturn(this, (Chart.__proto__ || Object.getPrototypeOf(Chart)).apply(this, arguments));
     }
-  }, {
-    key: "render",
-    value: function render() {
-      return React.createElement(
-        "div",
-        { className: "App" },
-        React.createElement(LineChart, { className: "lineChart", data: this.createFakeData() })
-      );
-    }
-  }]);
 
-  return App;
+    _createClass(Chart, [{
+        key: "generateChart",
+        value: function generateChart() {
+            console.log(this.props.chartData);
+            var data = this.props.chartData;
+            var minX = 0,
+                maxX = 0,
+                minY = 0,
+                maxY = 0;
+
+            for (key in data) {
+                if (minX == 0) {
+                    minX = data[key].x;
+                }
+                if (data[key].x < minX) {
+                    minX = data[key].x;
+                }
+            }
+            for (key in data) {
+                if (maxX == 0) {
+                    maxX = data[key].x;
+                }
+                if (data[key].x > maxX) {
+                    maxX = data[key].x;
+                }
+            }
+            for (key in data) {
+                if (minY == 0) {
+                    minY = data[key].y;
+                }
+                if (data[key].y < minY) {
+                    minY = data[key].y;
+                }
+            }
+            for (key in data) {
+                if (maxY == 0) {
+                    maxY = data[key].y;
+                }
+                if (data[key].y > maxY) {
+                    maxY = data[key].y;
+                }
+            }
+
+            var averageY = (maxY + minY) / 2;
+
+            var coordinatesList = data.map(function (d) {
+                var xCoordinate = (d.x - minX) / (maxX - minX) * 100;
+                xCoordinate = xCoordinate.toString();
+
+                var yCoordinate = (d.y - minY) / (maxY - minY) * 100;
+                yCoordinate = yCoordinate.toString();
+
+                // console.log(xCoordinate + "," + yCoordinate);
+                return xCoordinate + "," + yCoordinate;
+            });
+            //console.log(coordinatesList);
+
+            var coordinates = "";
+
+            for (point in coordinatesList) {
+                coordinates = coordinates + coordinatesList[point] + " ";
+                //console.log(point);
+            }
+            return coordinates;
+        }
+    }, {
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "svg",
+                { className: "chart" },
+                React.createElement("polyline", {
+                    className: "chartLine",
+                    fill: "none",
+                    stroke: "#0074d9",
+                    strokeWidth: "1",
+                    points: this.generateChart() })
+            );
+        }
+    }]);
+
+    return Chart;
 }(React.Component);
 
-var LineChart = function (_React$Component4) {
-  _inherits(LineChart, _React$Component4);
+var Deck = function (_React$Component3) {
+    _inherits(Deck, _React$Component3);
 
-  function LineChart() {
-    _classCallCheck(this, LineChart);
+    function Deck(props) {
+        _classCallCheck(this, Deck);
 
-    return _possibleConstructorReturn(this, (LineChart.__proto__ || Object.getPrototypeOf(LineChart)).apply(this, arguments));
-  }
+        var _this3 = _possibleConstructorReturn(this, (Deck.__proto__ || Object.getPrototypeOf(Deck)).call(this, props));
 
-  _createClass(LineChart, [{
-    key: "getMinX",
-
-
-    // GET MAX & MIN X
-    value: function getMinX() {
-      var data = this.props.data;
-
-      return data[0].x;
+        _this3.state = {
+            stocks: [{ ticker: "sfdfsd", name: "Very Long Company Name Incorporated", price: 123, percentChange: 4, changeType: "percentChangeUp" }, { ticker: "W", name: "Wayfair, Inc.", price: 123, percentChange: -2, changeType: "percentChangeDown" }, { ticker: "B", name: "B", price: 123 }, { ticker: "C", name: "C", price: 123 }, { ticker: "D", name: "D", price: 123 }, { ticker: "E", name: "E", price: 123 }, { ticker: "F", name: "F", price: 123 }, { ticker: "G", name: "G", price: 123 }]
+        };
+        _this3.getStockData();
+        return _this3;
     }
-  }, {
-    key: "getMaxX",
-    value: function getMaxX() {
-      var data = this.props.data;
 
-      return data[data.length - 1].x;
-    }
-    // GET MAX & MIN Y
+    _createClass(Deck, [{
+        key: "getStockData",
+        value: function getStockData() {
+            var _this4 = this;
 
-  }, {
-    key: "getMinY",
-    value: function getMinY() {
-      var data = this.props.data;
+            var symbols = "w,f,ba,aapl,fb,jnj";
 
-      return data.reduce(function (min, p) {
-        return p.y < min ? p.y : min;
-      }, data[0].y);
-    }
-  }, {
-    key: "getMaxY",
-    value: function getMaxY() {
-      var data = this.props.data;
+            var Http = new XMLHttpRequest();
+            var url = 'https://cloud.iexapis.com/stable/stock/market/batch?symbols=' + symbols + '&types=quote,chart&range=1d&token=pk_ea3fad39b66c4c08a98acce72eda2aaa';
+            Http.open("GET", url);
 
-      return data.reduce(function (max, p) {
-        return p.y > max ? p.y : max;
-      }, data[0].y);
-    }
-  }, {
-    key: "getSvgX",
-    value: function getSvgX(x) {
-      var svgWidth = this.props.svgWidth;
+            Http.onload = function (e) {
+                var stockData = JSON.parse(Http.responseText);
+                console.log(stockData);
+                var stocks = [];
+                for (var key in stockData) {
+                    var name = stockData[key].quote.companyName;
+                    var price = stockData[key].quote.latestPrice;
+                    var ticker = stockData[key].quote.symbol;
+                    var percentChange = (stockData[key].quote.latestPrice - stockData[key].quote.open) / stockData[key].quote.open;
+                    percentChange = Math.round(percentChange * 100 * 100) / 100;
+                    var changeType = percentChange >= 0 ? "percentChangeUp" : "percentChangeDown";
+                    var chartData = [];
+                    for (var key2 in stockData[key].chart) {
+                        var dateKey;
+                        dateKey = stockData[key].chart[key2].date + " " + stockData[key].chart[key2].minute;
+                        if (stockData[key].chart[key2].close != null) {
+                            chartData.push({ x: Date.parse(dateKey), y: stockData[key].chart[key2].close });
+                        }
+                    }
+                    var b = { name: name, price: price, ticker: ticker, percentChange: percentChange, changeType: changeType, chartData: chartData };
+                    stocks.push(b);
+                }
+                _this4.setState({ stocks: stocks });
+            };
 
-      return x / this.getMaxX() * svgWidth;
-    }
-  }, {
-    key: "getSvgY",
-    value: function getSvgY(y) {
-      var svgHeight = this.props.svgHeight;
+            Http.send();
+        }
+    }, {
+        key: "generateCards",
+        value: function generateCards() {
+            var stateValues = this.state.stocks;
+            var cardsList = stateValues.map(function (s) {
+                return React.createElement(Card, { key: s.ticker, name: s.name, price: s.price, percentChange: s.percentChange, changeType: s.changeType, chartData: s.chartData });
+            });
 
-      return svgHeight - y / this.getMaxY() * svgHeight;
-    }
-  }, {
-    key: "makeAxis",
-    value: function makeAxis() {
-      var minX = this.getMinX(),
-          maxX = this.getMaxX();
-      var minY = this.getMinY(),
-          maxY = this.getMaxY();
-      return React.createElement(
-        "g",
-        { className: "linechart_axis" },
-        React.createElement("line", {
-          x1: this.getSvgX(minX), y1: this.getSvgY(minY),
-          x2: this.getSvgX(maxX), y2: this.getSvgY(minY) }),
-        React.createElement("line", {
-          x1: this.getSvgX(minX), y1: this.getSvgY(minY),
-          x2: this.getSvgX(minX), y2: this.getSvgY(maxY) })
-      );
-    }
-  }, {
-    key: "makePath",
-    value: function makePath() {
-      var _this6 = this;
+            return cardsList;
+        }
+    }, {
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                { className: "deck" },
+                this.generateCards()
+            );
+        }
+    }]);
 
-      var _props = this.props,
-          data = _props.data,
-          color = _props.color;
-
-      var pathD = "M " + this.getSvgX(data[0].x) + " " + this.getSvgY(data[0].y) + " ";
-      pathD += data.map(function (point, i) {
-        return "L " + _this6.getSvgX(point.x) + " " + _this6.getSvgY(point.y) + " ";
-      });
-      return React.createElement("path", { className: "linechart_path", d: pathD, style: { stroke: color } });
-    }
-  }, {
-    key: "render",
-    value: function render() {
-      var _props2 = this.props,
-          svgHeight = _props2.svgHeight,
-          svgWidth = _props2.svgWidth;
-
-      return React.createElement(
-        "svg",
-        { className: "lineChart", viewBox: "0 0 " + svgWidth + " " + svgHeight },
-        this.makePath()
-      );
-    }
-  }]);
-
-  return LineChart;
+    return Deck;
 }(React.Component);
 
-LineChart.defaultProps = {
-  data: [{ x: 1, y: 1 }, { x: 2, y: 2 }],
-  color: '#2196F3',
-  svgHeight: 100,
-  svgWidth: 300
+Card.defaultProps = {
+    chartData: [{ x: 1, y: 1 }, { x: 2, y: 2 }, { x: 3, y: 1 }, { x: 4, y: 0 }]
 };
 
 ReactDOM.render(React.createElement(Deck, null), document.getElementById('root'));
--- src/index.js
+++ src/index.js
@@ -9,11 +9,89 @@ class Card extends React.Component {
             <h1 className="companyName">{this.props.name}</h1>
             <h6 className="currentPrice">${this.props.price}</h6>
             <h6 className={this.props.changeType}>&nbsp; ({this.props.percentChange}%)</h6>
-            <div><App chartData ={this.props.chartData}/></div>
+            <Chart chartData = {this.props.chartData} />
         </div>
      )
  }
 }
+
+class Chart extends React.Component {
+ 
+    generateChart(){
+        console.log(this.props.chartData);
+        var data = this.props.chartData;
+        var minX = 0, maxX = 0, minY = 0, maxY = 0;
+
+        for(key in data){
+            if(minX == 0){
+                minX = data[key].x;
+            }
+            if(data[key].x < minX){
+                minX = data[key].x;
+            }
+        }
+        for(key in data){
+            if(maxX == 0){
+                maxX = data[key].x;
+            }
+            if(data[key].x > maxX){
+                maxX = data[key].x;
+            }
+        }
+        for(key in data){
+            if(minY == 0){
+                minY = data[key].y;
+            }
+            if(data[key].y < minY){
+                minY = data[key].y;
+            }
+        }
+        for(key in data){
+            if(maxY == 0){
+                maxY = data[key].y;
+            }
+            if(data[key].y > maxY){
+                maxY = data[key].y;
+            }
+        }
+        
+        var averageY = (maxY + minY) / 2;
+
+        var coordinatesList = data.map(function(d){
+            var xCoordinate = ((d.x - minX) / (maxX - minX)) * 100;
+            xCoordinate = xCoordinate.toString();
+
+            var yCoordinate = ((d.y - minY) / (maxY - minY)) * 100;
+            yCoordinate = yCoordinate.toString();
+
+           // console.log(xCoordinate + "," + yCoordinate);
+            return xCoordinate + "," + yCoordinate;
+            
+        });
+        //console.log(coordinatesList);
+
+        var coordinates = "";
+
+        for(point in coordinatesList){
+            coordinates = coordinates + coordinatesList[point] + " ";
+            //console.log(point);
+        }
+        return coordinates;
+    }
+
+    render(){
+        return(
+            <svg className="chart">
+            <polyline
+                className = "chartLine"
+               fill="none"
+               stroke="#0074d9"
+               strokeWidth="1"
+               points={this.generateChart()}/>
+          </svg>
+        )
+    };
+   }
 
 class Deck extends React.Component {
     constructor(props) {
@@ -59,11 +137,10 @@ class Deck extends React.Component {
             for(var key2 in stockData[key].chart){
                 var dateKey;
                 dateKey = stockData[key].chart[key2].date + " " + stockData[key].chart[key2].minute;
-                //console.log(Date.parse(stockData[key].chart[key2].date));
-                //console.log(Date.parse(dateKey));
+                if(stockData[key].chart[key2].close != null){
                 chartData.push({x: Date.parse(dateKey), y: stockData[key].chart[key2].close})
+                }
             }
-            console.log(chartData);
             var b = {name: name,price: price,ticker: ticker,percentChange: percentChange, changeType: changeType, chartData: chartData};
             stocks.push(b);
         }
@@ -87,110 +164,18 @@ class Deck extends React.Component {
     render(){
     return <div className="deck">{this.generateCards()}</div>
  }
+
+ 
 }
 
-
-class App extends React.Component {
-  createFakeData(){
-    // This function creates data that doesn't look entirely random
-    var data = []
-    for (let x = 0; x <= 30; x++) {
-      const random = Math.random();
-      const temp = data.length > 0 ? data[data.length-1].y : 50;
-      const y = random >= .45 ? temp + Math.floor(random * 20) : temp - Math.floor(random * 20);
-      data.push({x,y})
-    }
-    // data = [
-    //     {x:0, y:0},
-    //     {x:1,y:1},
-    //     {x:3,y:-1}
-    // ]
-    return data;
-  }
-  render() {
-    return (
-      <div className="App">
-        <LineChart className="lineChart" data={this.createFakeData()} />
-      </div>
-    );
-  }
-}
-
-class LineChart extends React.Component {
-
-    // GET MAX & MIN X
-    getMinX() {
-        const {data} = this.props;
-        return data[0].x;
-      }
-      getMaxX() {
-        const {data} = this.props;
-        return data[data.length - 1].x;
-      }
-      // GET MAX & MIN Y
-      getMinY() {
-        const {data} = this.props;
-        return data.reduce((min, p) => p.y < min ? p.y : min, data[0].y);
-      }
-      getMaxY() {
-        const {data} = this.props;
-        return data.reduce((max, p) => p.y > max ? p.y : max, data[0].y);
-      }
-    
-      getSvgX(x) {
-        const {svgWidth} = this.props;
-        return (x / this.getMaxX() * svgWidth);
-      }
-      getSvgY(y) {
-        const {svgHeight} = this.props;
-        return svgHeight - (y / this.getMaxY() * svgHeight);
-      }
-    
-      makeAxis() {
-        const minX = this.getMinX(), maxX = this.getMaxX();
-        const minY = this.getMinY(), maxY = this.getMaxY();
-      return (
-          <g className="linechart_axis">
-            <line
-              x1={this.getSvgX(minX)} y1={this.getSvgY(minY)}
-              x2={this.getSvgX(maxX)} y2={this.getSvgY(minY)} />
-            <line
-              x1={this.getSvgX(minX)} y1={this.getSvgY(minY)}
-              x2={this.getSvgX(minX)} y2={this.getSvgY(maxY)} />
-          </g>
-          );
-        }
-        makePath() {
-            const {data, color} = this.props;
-            let pathD = "M " + this.getSvgX(data[0].x) + " " + this.getSvgY(data[0].y) + " ";
-        pathD += data.map((point, i) => {
-              return "L " + this.getSvgX(point.x) + " " + this.getSvgY(point.y) + " ";
-            });
-        return (
-          <path className="linechart_path" d={pathD} style={{stroke: color}} />
-            );
-          }
-        render() {
-            const {svgHeight, svgWidth} = this.props;
-        return (
-              <svg className = "lineChart" viewBox={`0 0 ${svgWidth} ${svgHeight}`}>
-                {this.makePath()}
-                {/* {this.makeAxis()} */}
-              </svg>
-            );
-          }
-    }
-    LineChart.defaultProps = {
-      data: [
-          {x: 1, y:1},
-          {x:2, y:2}
-      ],  
-      color: '#2196F3',  
-      svgHeight: 100,  
-      svgWidth: 300
-    }
-
-
+Card.defaultProps = {
+    chartData:[
+        {x:1,y:1},
+        {x:2,y:2},
+        {x:3,y:1},
+        {x:4,y:0}
+    ]
+  };
 
 
 ReactDOM.render(<Deck />, document.getElementById('root'));
