3d77bd5e15ec819856ab0beb1e9630d63bb7bd1f
rahulshahaha <getrshah@gmail.com>
2019-08-11 22:34:19 -0400

    x

--- index.js
+++ index.js
@@ -222,8 +222,3 @@ Card.defaultProps = {
 };
 
 ReactDOM.render(React.createElement(Deck, null), document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
